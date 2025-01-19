293642dfc3ded5d20dd80fccdb13692665e4b6c0
Quince <quinceroms@gmail.com>
2025-01-19 19:09:26 +0100

    Fixup! guacamoleb: Fixed path

--- lineage_guacamoleb.mk
+++ lineage_guacamoleb.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/guacamoleb/device.mk)
 
 # Inherit some common RisingOS stuff.
-$(call inherit-product, vendor/linegae/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_guacamoleb
